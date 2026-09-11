6edf5773fcbb0bfc2e8872cde9a416ea02de3933
bruh <maksimkaqq677@gmail.com>
2026-09-11 18:41:25 +0300

    Bump version to 0.5.0

--- remnawave_customizer/__init__.py
+++ remnawave_customizer/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 
 # Install the small Remnawave frontend compatibility layer before CLI modules
 # import themes/proxy functions. It only changes our generated CSS/proxy config;
